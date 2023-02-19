199c9a3f84ff87a4861611a760163832e9230c12
AhmedAli9000 <85993466+AhmedAli9000@users.noreply.github.com>
2023-02-19 23:58:42 +0200

    Update DCSetup.ps1

--- PowerShell/DCSetup.ps1
+++ PowerShell/DCSetup.ps1
@@ -9,9 +9,9 @@ choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
 #Download Scripts to Set the rest of the Domain up when logged in
-New-Item -Path "c:\" -Name "BaselabSetup" -ItemType "directory" -Force
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/PowerShell/baselab_DomainSetup.ps1" -OutFile "C:\BaselabSetup\baselab_DomainSetup.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/PowerShell/baselab_LabStructure.ps1" -OutFile "C:\BaselabSetup\baselab_LabStructure.ps1"
+New-Item -Path "c:\" -Name "DomainSetup" -ItemType "directory" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AhmedAli9000/repo01/main/PowerShell/DomainSetup.ps1" -OutFile "C:\DomainSetup\DomainSetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/AhmedAli9000/repo01/main/PowerShell/LabStructure.ps1" -OutFile "C:\DomainSetup\LabStructure.ps1"
 #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
 #Allow Ping
