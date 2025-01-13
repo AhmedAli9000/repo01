f0e4b519c0915f73dba42fa2c39dc9e439e4abc5
AhmedAli9000 <85993466+AhmedAli9000@users.noreply.github.com>
2025-01-13 16:30:59 +0200

    Update sysprep.ps1

--- sysprep.ps1
+++ sysprep.ps1
@@ -1,31 +1,23 @@
-# Script to run Sysprep on a VM
-# Logging is handy when you need it!
-try {
-    if (-not (Test-Path 'C:\logfiles')) {
-        New-Item -ItemType Directory -Path 'C:\logfiles' | Out-Null
-    }
-} catch {
-    Write-Output "Error creating log directory: $_" | Out-File -Encoding utf8 "C:\Temp\script_errors.log" -Append
-}
 
-$logFile = "C:\logfiles\" + (Get-Date -Format 'yyyyMMdd') + '_softwareinstall.log'
+#Script to run Sysprep on a VM
+#Logging is handy when you need it!
+if ((test-path c:\logfiles) -eq $false) {
+    new-item -ItemType Directory -path 'c:\' -name 'logfiles' | Out-Null
+} 
+$logFile = "c:\logfiles\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
 
 # Logging function
 function Write-Log {
     Param($message)
-    $logEntry = "$(Get-Date -Format 'yyyyMMdd HH:mm:ss') $message"
-    Write-Output $logEntry
-    $logEntry | Out-File -Encoding utf8 $logFile -Append
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
 }
-
-# Run Sysprep
-try {
-    Write-Log "Sysprep Starting"
-    Start-Process -FilePath 'C:\Windows\System32\sysprep\sysprep.exe' -ArgumentList '/generalize', '/oobe', '/shutdown' -ErrorAction Stop -Wait
-    Write-Log "Sysprep completed successfully"
-    exit 0
-} catch {
-    $ErrorMessage = $_.Exception.Message
-    Write-Log "Error running Sysprep: $ErrorMessage"
-    exit 1
+#Run Sysprep
+try{
+    write-output "Sysprep Starting"
+    # Start-Process -filepath 'c:\Windows\system32\sysprep\sysprep.exe' -ErrorAction Stop -ArgumentList '/generalize', '/oobe', '/mode:vm', '/shutdown'
+    Start-Process -filepath 'c:\Windows\system32\sysprep\sysprep.exe' -ErrorAction Stop -ArgumentList '/generalize', '/oobe', '/shutdown'
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error running Sysprep: $ErrorMessage"
 }
